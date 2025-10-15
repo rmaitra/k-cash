165868448296e49378f929e4a69d40df983e39c0
Raj <raj.d.maitra@gmail.com>
2025-10-15 14:20:22 -0500

    update

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve static files from a 'public' directory (where index.html would reside)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Catch-all route to serve index.html for all other requests (useful for SPAs)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
