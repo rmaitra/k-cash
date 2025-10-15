8f5d328dcaa4859697d27cdfd8fa3553892f744d
Raj <raj.d.maitra@gmail.com>
2025-10-15 14:29:52 -0500

    added proc

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; 
 
 // Serve static files from a 'public' directory (where index.html would reside)
 app.use(express.static(path.join(__dirname, 'public')));
